2d74e3cc6f41073b09d742b892aa3b66ea746268
jatin-sofocle <jatin.agrawal@sofocle.com>
2018-09-21 16:45:50 +0530

    fetch by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,8 +29,28 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
-    res.status(400).send(e);  
+    res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(todo){
+      res.send(todo);
+    }else{
+      res.status(404).send();
+    }
+  },(e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
